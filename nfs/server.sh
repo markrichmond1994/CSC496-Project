3fc282417ea2967d74c663e8d4640b06e8452585
markrichmond1994 <60485080+markrichmond1994@users.noreply.github.com>
2020-05-08 21:29:10 -0400

    Update server.sh

--- nfs/server.sh
+++ nfs/server.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+#docker swarm server
+sudo dpkg -l | grep nfs-kernel-server
+sudo mkdir /var/nfs
+sudo chown nobody:nogroup /var/nfs
+
+cat /etc/exports
+/var/nfs    *(rw,sync,no_subtree_check)
+
+sudo chown nobody:nogroup /var/nfs
+sudo exportfs -a
+sudo service nfs-kernel-server start
